fe25d1355874fa57053c9bec6ca0e2e64eb4caa4
sahudevaryan17-ux <sahudevaryan17@gmail.com>
2026-02-26 17:32:05 +0000

    add

---
+++ Javascript/02_datatypes.js
@@ -0,0 +1,7 @@
+let n = "devaryan";
+const age = 20;
+let isNice = true;
+let kuchnhi;
+ typeof (n, age , isNice);
+console.table([typeof(n),typeof(age),typeof(isNice), typeof(kuchnhi), typeof(null)]);
+//typeof of null is object but undefined is undefined; 
